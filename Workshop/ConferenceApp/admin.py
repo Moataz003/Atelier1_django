a319874a1bee5f0dfec596973eb3d6a8c246dd05
Moataz003 <brahmi.motaz@gmail.com>
2025-10-16 14:44:01 +0100

    actions
    deuxieme commit

--- Workshop/ConferenceApp/admin.py
+++ Workshop/ConferenceApp/admin.py
@@ -1,8 +1,5 @@
-
 from django.contrib import admin
 from .models import Conference, Submission
-
-# Personnalisation des en-têtes de l'admin
 admin.site.site_header = "Gestion des conférences"
 admin.site.site_title = "Admin Conférences"
 admin.site.index_title = "Tableau de bord des conférences"
@@ -47,14 +44,38 @@ class ConferenceAdmin(admin.ModelAdmin):
 # Admin Submission
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ("title", "status", "user", "conference", "submission_date", "short_abstract")
-    list_filter = ("status", "conference", "submission_date")
+    list_display = ("title", "status", "user", "conference", "submission_date", "payed", "short_abstract")
+    list_filter = ("status", "payed", "conference", "submission_date")
     search_fields = ("title", "keywords", "user__username")
+    list_editable = ("status", "payed")
     readonly_fields = ("submission_id", "submission_date")
+    fieldsets = (
+        ("Infos générales", {
+            "fields": ("submission_id", "title", "abstract", "keywords")
+        }),
+        ("Fichier et conférence", {
+            "fields": ("paper", "conference")
+        }),
+        ("Suivi", {
+            "fields": ("status", "payed", "submission_date", "user")
+        }),
+    )
+
+    actions = ["mark_as_payed", "accept_submissions"]
 
     def short_abstract(self, obj):
         return (obj.abstract[:50] + "...") if obj.abstract and len(obj.abstract) > 50 else obj.abstract
     short_abstract.short_description = "Résumé court"
+
+    def mark_as_payed(self, request, queryset):
+        updated = queryset.update(payed=True)
+        self.message_user(request, f"{updated} soumissions marquées comme payées.")
+    mark_as_payed.short_description = "Marquer comme payées"
+
+    def accept_submissions(self, request, queryset):
+        updated = queryset.update(status="accepted")
+        self.message_user(request, f"{updated} soumissions acceptées.")
+    accept_submissions.short_description = "Accepter les soumissions"
 
 
 
--- Workshop/ConferenceApp/models.py
+++ Workshop/ConferenceApp/models.py
@@ -2,8 +2,6 @@ from ast import Import
 from django.db import models
 from django.core.validators import MinLengthValidator 
 from django.core.exceptions import ValidationError
-
-# Create your models here.
 class Conference(models.Model):
     conference_id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
@@ -40,6 +38,7 @@ class Submission(models.Model):
         ("rejected","rejected")]
 
     status=models.CharField(max_length=255,choices=Choises)
+    payed=models.BooleanField(default=False)
     submission_date=models.DateTimeField(auto_now_add=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
