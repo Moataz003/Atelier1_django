8c264aaff14e89f31fd5f4fa46a63cfb16072bcb
Moataz003 <brahmi.motaz@gmail.com>
2025-10-23 14:04:32 +0100

    create conf
    create conf

--- Workshop/ConferenceApp/views.py
+++ Workshop/ConferenceApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from .models import Conference
 
@@ -10,5 +11,13 @@ class ConferenceDetails(DetailView):
     model = Conference
     template_name = "conference_detail.html"
     context_object_name = "conference"
+
+class ConferenceCreate(CreateView):
+    model = Conference
+    fields = ['name', 'description', 'theme', 'start_date', 'end_date']
+    # Form validation
+    template_name = "conference_form.html"
+    # Form validation and redirection
+    success_url = reverse_lazy("conference_list")
 
 
